96b9bc5c6231b35dc1b5bcac94ba9031ec3b613c
AirBurn <rosan1233456@gmail.com>
2024-08-09 22:40:08 +0300

    nei waila

---
+++ src/main/java/com/pyding/deathlyhallows/integrations/NEIDHConfig.java
@@ -0,0 +1,26 @@
+package com.pyding.deathlyhallows.integrations;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import com.pyding.deathlyhallows.DeathlyHallows;
+import com.pyding.deathlyhallows.blocks.DHBlocks;
+import net.minecraft.item.ItemStack;
+
+public class NEIDHConfig implements IConfigureNEI {
+	
+	@Override
+	public void loadConfig() {
+		API.hideItem(new ItemStack(DHBlocks.elderRitualRune));
+	}
+
+	@Override
+	public String getName() {
+		return DeathlyHallows.NAME;
+	}
+
+	@Override
+	public String getVersion() {
+		return DeathlyHallows.VERSION;
+	}
+	
+}
