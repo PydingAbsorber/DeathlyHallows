5a1e0c4c5f6873cff70232be27357ba1c6943ee1
AirBurn <rosan1233456@gmail.com>
2024-08-10 00:47:26 +0300

    refactor RenderAbsoluteDeath

--- src/main/java/com/pyding/deathlyhallows/render/entity/RenderAbsoluteDeath.java
+++ src/main/java/com/pyding/deathlyhallows/render/entity/RenderAbsoluteDeath.java
@@ -1,7 +1,9 @@
 package com.pyding.deathlyhallows.render.entity;
 
 import com.emoniph.witchery.client.model.ModelDeath;
+import com.pyding.deathlyhallows.DeathlyHallows;
 import com.pyding.deathlyhallows.entities.EntityAbsoluteDeath;
+import com.pyding.deathlyhallows.integrations.DHIntegration;
 import com.pyding.deathlyhallows.render.ModelWrapperDisplayList;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -19,27 +21,25 @@ import org.lwjgl.opengl.GL11;
 @SideOnly(Side.CLIENT)
 public class RenderAbsoluteDeath extends RenderLiving {
 	private float modelRotation;
-
-	private static final ResourceLocation TEXTURE_URL = new ResourceLocation("witchery", "textures/entities/death.png");
-
-	private static final ResourceLocation absolute = new ResourceLocation("dh", "textures/particles/absolute_block.png");
-	private static final ResourceLocation earth = new ResourceLocation("dh", "textures/particles/earth_block.png");
-	private static final ResourceLocation fire = new ResourceLocation("dh", "textures/particles/fire_block.png");
-	private static final ResourceLocation generic = new ResourceLocation("dh", "textures/particles/generic_block.png");
-	private static final ResourceLocation magic = new ResourceLocation("dh", "textures/particles/magic_block.png");
-	private static final ResourceLocation poison = new ResourceLocation("dh", "textures/particles/poison_block.png");
-	private static final ResourceLocation dark = new ResourceLocation("dh", "textures/particles/void_block.png");
-	private static final ResourceLocation water = new ResourceLocation("dh", "textures/particles/water_block.png");
-
-	private final ResourceLocation modelPath = new ResourceLocation("dh", "models/generic_block.obj");
+	private static final ResourceLocation
+			modelPath = new ResourceLocation(DeathlyHallows.MODID, "models/generic_block.obj"),
+			TEXTURE_URL = new ResourceLocation(DHIntegration.WITCHERY, "textures/entities/death.png"),
+			absolute = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/absolute_block.png"),
+			earth = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/earth_block.png"),
+			fire = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/fire_block.png"),
+			generic = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/generic_block.png"),
+			magic = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/magic_block.png"),
+			poison = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/poison_block.png"),
+			dark = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/void_block.png"),
+			water = new ResourceLocation(DeathlyHallows.MODID, "textures/particles/water_block.png");
 	private final IModelCustom shieldModel = new ModelWrapperDisplayList((WavefrontObject)AdvancedModelLoader.loadModel(modelPath));
 
 	public RenderAbsoluteDeath() {
 		super(new ModelDeath(), 0.5F);
 	}
 
-	public void doRenderDeath(EntityAbsoluteDeath entity, double par2, double par4, double par6, float par8, float par9) {
-		super.doRender(entity, par2, par4, par6, par8, par9);
+	public void doRenderDeath(EntityAbsoluteDeath entity, double x, double y, double z, float yaw, float partial) {
+		super.doRender(entity, x, y, z, yaw, partial);
 		BossStatus.setBossStatus(entity, true);
 		modelRotation += 1.0F;
 		int shieldAmount = 100;
@@ -47,7 +47,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("absoluteblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(absolute);
-			GL11.glTranslatef((float)par2 + 1, (float)par4 + 0.5F, (float)par6);
+			GL11.glTranslatef((float)x + 1, (float)y + 0.5F, (float)z);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -55,7 +55,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("earthblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(earth);
-			GL11.glTranslatef((float)par2 - 1, (float)par4 + 0.5F, (float)par6);
+			GL11.glTranslatef((float)x - 1, (float)y + 0.5F, (float)z);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -63,7 +63,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("fireblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(fire);
-			GL11.glTranslatef((float)par2 + 1, (float)par4 + 0.5F, (float)par6 + 1);
+			GL11.glTranslatef((float)x + 1, (float)y + 0.5F, (float)z + 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -71,7 +71,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("genericblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(generic);
-			GL11.glTranslatef((float)par2 - 1, (float)par4 + 0.5F, (float)par6 + 1);
+			GL11.glTranslatef((float)x - 1, (float)y + 0.5F, (float)z + 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -79,7 +79,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("magicblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(magic);
-			GL11.glTranslatef((float)par2 + 1, (float)par4 + 0.5F, (float)par6 - 1);
+			GL11.glTranslatef((float)x + 1, (float)y + 0.5F, (float)z - 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -87,7 +87,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("witherblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(poison);
-			GL11.glTranslatef((float)par2 - 1, (float)par4 + 0.5F, (float)par6 - 1);
+			GL11.glTranslatef((float)x - 1, (float)y + 0.5F, (float)z - 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -95,7 +95,7 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("voidblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(dark);
-			GL11.glTranslatef((float)par2, (float)par4 + 0.5F, (float)par6 + 1);
+			GL11.glTranslatef((float)x, (float)y + 0.5F, (float)z + 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
@@ -103,24 +103,20 @@ public class RenderAbsoluteDeath extends RenderLiving {
 		if(nbt.getInteger("waterblock") >= shieldAmount) {
 			GL11.glPushMatrix();
 			bindTexture(water);
-			GL11.glTranslatef((float)par2, (float)par4 + 0.5F, (float)par6 - 1);
+			GL11.glTranslatef((float)x, (float)y + 0.5F, (float)z - 1);
 			GL11.glRotatef(modelRotation, 0.0F, 1.0F, 0.0F);
 			shieldModel.renderAll();
 			GL11.glPopMatrix();
 		}
 	}
 
-	public void doRender(EntityLiving entity, double par2, double par4, double par6, float par8, float par9) {
-		this.doRenderDeath((EntityAbsoluteDeath)entity, par2, par4, par6, par8, par9);
+	public void doRender(EntityLiving entity, double x, double y, double z, float yaw, float partial) {
+		this.doRenderDeath((EntityAbsoluteDeath)entity, x, y, z, yaw, partial);
 	}
 
-	protected ResourceLocation getEntityTexture(Entity par1Entity) {
-		return this.func_110832_a((EntityAbsoluteDeath)par1Entity);
-	}
-
-	protected ResourceLocation func_110832_a(EntityAbsoluteDeath par1Entity) {
+	protected ResourceLocation getEntityTexture(Entity e) {
 		return TEXTURE_URL;
 	}
-
+	
 }
 
